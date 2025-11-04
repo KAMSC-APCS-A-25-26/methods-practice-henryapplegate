325b1e970e5768e154b2b768f231d2a1d8f45094
henryapplegate <ha7applegate@gmail.com>
2025-11-04 13:43:26 -0500

    Update TripPlanner.java

--- src/main/java/TripPlanner.java
+++ src/main/java/TripPlanner.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class TripPlanner {
     public static void main(String[] args) {
@@ -7,7 +8,22 @@ public class TripPlanner {
         // - Average speed (mph)
         // - Fuel efficiency (miles per gallon)
         // - Fuel price per gallon
-        
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        System.out.print("Enter trip distance (miles): ");
+        double distance = sc.nextDouble();
+        System.out.print("Enter avergage speed (mph): ");
+        double speed = sc.nextDouble();
+        System.out.print("Enter fuel efficiency (miles per gallon): ");
+        double efficiency = sc.nextDouble();
+        System.out.print("Enter fuel price per gallon ($): ");
+        double price = sc.nextDouble();
+        String time = df.format(calculateTravelTime(distance, speed));
+        String fuel = df.format(calculateFuelNeeded(distance, efficiency));
+        String cost = df.format(calculateTripCost(distance, efficiency, price));
+        displayResults(time, fuel, cost);
+
         // TODO: Calculate travel time using a return method
         
         // TODO: Calculate fuel needed using a return method
@@ -19,11 +35,25 @@ public class TripPlanner {
     }
     
     // TODO: Implement calculateTravelTime method
+    public static double calculateTravelTime(double distance, double speed){
+        return distance/speed;
+    }
     
     // TODO: Implement calculateFuelNeeded method
+    public static double calculateFuelNeeded(double distance, double efficiency){
+        return distance/efficiency;
+    }
     
     // TODO: Implement calculateTripCost method
-    
+    public static double calculateTripCost(double distance, double efficiency, double price){
+        return (distance/efficiency) * price;
+    }
     // TODO: Implement displayResults method
+    public static void displayResults(String time, String fuel, String cost){
+        System.out.println("\nResults:");
+        System.out.println("Travel Time: " + time + " hours");
+        System.out.println("Fuel Needed: " + fuel + " gallons");
+        System.out.println("Trip Cost: $" + cost);
+    }
     
 }
